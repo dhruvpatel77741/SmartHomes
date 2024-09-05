e6925f2422af72c20671be3fa6c60992e372853f
dhruvpatel77741 <dhruvpatel77741@gmail.com>
2024-09-04 22:58:50 -0500

    Login Page Functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Login/>}/>
       <Route path='/signup' element={<SignUp/>}/>
+      <Route path='dashboard' element={<Dashboard/>}/>
     </Routes>
   );
 }
