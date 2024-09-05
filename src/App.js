09189ae97b2b7f87abfead9c153aea9e99d20d83
Dhruv Patel <77951610+dhruvpatel77741@users.noreply.github.com>
2024-09-04 22:39:55 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Login/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
     </Routes>
   );
 }
