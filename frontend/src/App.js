bc11f1fa0f2f8eee4d9579c1b1b5ae81699e9f6a
dhruvpatel77741 <dhruvpatel77741@gmail.com>
2024-09-09 10:08:46 -0500

    Private Routing Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,78 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import Dashboard from './components/Dashboard';
-import CustomerList from './components/CustomerList';
-import ProductsList from './components/ProductsList';
-import ProductDetails from './components/ProductDetails';
-import Cart from './components/Cart';
-import Checkout from './components/Checkout';
-import Payment from './components/Payment';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Dashboard from "./components/Dashboard";
+import CustomerList from "./components/CustomerList";
+import ProductsList from "./components/ProductsList";
+import ProductDetails from "./components/ProductDetails";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import Payment from "./components/Payment";
+import PrivateRoute from "./components/PrivateRoute"; // Import PrivateRoute
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Login/>}/>
-      <Route path='/signup' element={<SignUp/>}/>
-      <Route path='/dashboard' element={<Dashboard/>}/>
-      <Route path='/dashboard/product/:id' element={<ProductDetails/>}/>
-      <Route path='/customer-list' element={<CustomerList/>}/>
-      <Route path='/product-list' element={<ProductsList/>}/>
-      <Route path='/cart' element={<Cart/>}/>
-      <Route path='/checkout' element={<Checkout/>}/>
-      <Route  path='/payment' element={<Payment/>}/>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/dashboard/product/:id"
+        element={
+          <PrivateRoute>
+            <ProductDetails />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/customer-list"
+        element={
+          <PrivateRoute>
+            <CustomerList />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/product-list"
+        element={
+          <PrivateRoute>
+            <ProductsList />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/cart"
+        element={
+          <PrivateRoute>
+            <Cart />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/checkout"
+        element={
+          <PrivateRoute>
+            <Checkout />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/payment"
+        element={
+          <PrivateRoute>
+            <Payment />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 }
---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const PrivateRoute = ({ children }) => {
+  const userId = localStorage.getItem('userId');
+
+  if (!userId) {
+    return <Navigate to="/" />;
+  }
+  return children;
+};
+
+export default PrivateRoute;
